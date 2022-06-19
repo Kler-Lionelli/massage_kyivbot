9c6a3de87e35be9f2b07a85330353b2d58c3d65f
Kler-Lionelli <kler.lionelli@gmail.com>
2022-06-19 12:31:42 +0300

    first commit

---
+++ const.js
@@ -0,0 +1,22 @@
+const commands = `
+/start - Перезапуск бота
+/help - Допомога
+`
+const text1 = `
+1 Текст для перевірки обробника і <a href="https://youtube.com/">Ютуб</a>
+`
+const text2 = `
+2 Текст для перевірки обробника і <a href="https://youtube.com/">Ютуб</a>
+`
+const text3 = `
+3 Текст для перевірки обробника і <a href="https://youtube.com/">Ютуб</a>
+`
+const text4 = `
+4 Текст для перевірки обробника і <a href="https://youtube.com/">Ютуб</a>
+`
+
+module.exports.commands = commands
+module.exports.text1 = text1
+module.exports.text2 = text2
+module.exports.text3 = text3
+module.exports.text4 = text4
